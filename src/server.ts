855bba5213d7670014bdbf8cb6ebd1b59896f440
alex ferreira da silva <hatealex100@gmail.com>
2023-04-26 19:34:42 -0300

    base created

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotevn from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import mainRoutes from "./routes/index";
 dotevn.config();
 
 const app = express();
@@ -13,5 +14,11 @@ app.engine("mustache", mustache());
 app.use(express.static(path.join(__dirname, "../public")));
 
 // routes
+app.use(mainRoutes);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found.");
+});
+
 
 app.listen(process.env.PORT || 4000, () => console.log("SERVER IS ONLINE"));
